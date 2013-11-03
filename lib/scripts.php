1351db2fa1277b0f0738e1e2c58ff399968b70b9
m4ximvs <m@ximvs.com>
2013-11-03 01:52:20 +0100

    boowp addon -  jquery:regex selector

--- lib/scripts.php
+++ lib/scripts.php
@@ -27,7 +27,7 @@ function roots_scripts() {
   }
 
   wp_register_script('modernizr', get_template_directory_uri() . '/assets/js/vendor/modernizr-2.6.2.min.js', false, null, false);
-  wp_register_script('roots_scripts', get_template_directory_uri() . '/assets/js/scripts.min.js', false, 'ad729fdc56df49272ff0779bdd5256fa', true);
+  wp_register_script('roots_scripts', get_template_directory_uri() . '/assets/js/scripts.min.js', false, '94856c658fec386ace3275e16d98163b', true);
   wp_enqueue_script('modernizr');
   wp_enqueue_script('jquery');
   wp_enqueue_script('roots_scripts');
